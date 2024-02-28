60fc49c38da162e29a087498fc67e2a2ca49f987
Tanito <tanomicheli99@gmail.com>
2024-02-28 17:58:46 -0300

    Fixed Errors

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -12,13 +12,13 @@ const AnimatedNumbers = dynamic(
 const achievementsList = [
     {
         metric: "Projects",
-        value: "100",
+        value: "10",
         postfix: "+",
     },
     {
-        prefix: "~",
+        prefix: "+",
         metric: "Users",
-        value: "100,000",
+        value: "100",
     },
     {
         metric: "Awards",
